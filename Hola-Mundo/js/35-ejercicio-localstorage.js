a20198c02b61d5679243980d2682046ab75f9cc7
MARIA MORENO <mariale_263@hotmail.com>
2019-04-14 11:54:50 +0200

    LocalStorage

--- Hola-Mundo/js/35-ejercicio-localstorage.js
+++ Hola-Mundo/js/35-ejercicio-localstorage.js
@@ -4,18 +4,22 @@
     /*
     formulario que nos permita añadir peliculas
     */
-    var formulario = document.querySelector('#formpeliculas');
     
-    formulario.addEventListener('submit', function(){
+      
+   var formulario = document.querySelector("#formpeliculas");
+
+   
+        formulario.addEventListener('submit', function(){
         
         var titulo = document.querySelector('#addpelicula').value;
         
-        if(titulo.lengh >= 1){//para que no me lo agregue vacio
+        //para que no me lo agregue vacio hago la decision
+        if(titulo.length >= 1 ){  
             localStorage.setItem(titulo, titulo);
-        }        
+        }      
     });
     
-/*
+
     var ul = document.querySelector('#peliculas-list');
     
     for (var i in localStorage) {
@@ -27,5 +31,21 @@
         ul.append(li);
         }      
 
-    }*/
+    }
+
+    //Borrar peliculas del formulario
+    var formulariob = document.querySelector("#formBorrarPeliculas");
+
+   
+    formulariob.addEventListener('submit', function(){
+    
+    var titulo = document.querySelector('#borrarPelicula').value;
+    
+    //para que no me lo agregue vacio hago la decision
+    if(titulo.length >= 1 ){  
+        localStorage.removeItem(titulo, titulo);
+    }      
+});
+
+
 
